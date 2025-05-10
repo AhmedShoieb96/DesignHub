f30ca891fa437152d9f576639b3eb07e7a615d1b
ahmed shoieb <ahmd.shoiib@gmail.com>
2025-05-10 19:14:34 +0300

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 
 import './App.css'
 import HomeSideBar from './assets/components/sideBar/SideBar'
-
+import SimpleSlideshow from './assets/components/sideBar/slideShow/Slider'  
 function App() {
   
 
   return (
     <>
      <HomeSideBar />
+     <SimpleSlideshow />
     </>
   )
 }
