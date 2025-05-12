5301876a91fbd410ce842911bcc68a4dcf3beced
Abdelaziz Emad <abdelazizemad1994@gmail.com>
2025-05-12 22:12:53 +0300

    Updated Image Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
+// Updated App.jsx
 // import { useState } from 'react'
-
 import './App.css'
 import HomeSideBar from './assets/components/navBar/NavBar'
 import PhoneIcon from './assets/components/phoneIcon/PhoneIcon'
 import SimpleSlideshow from './assets/components/slideShow/Slider' 
 import WhatsAppIcon from './assets/components/watsAppIcon/WhatsAppIcon'
 import ImageSlider from './assets/components/imagesSlideShow/ImageSlider'
-function App() {
-  
 
+function App() {
   return (
     <>
-     <HomeSideBar />
-     <SimpleSlideshow />
-     <WhatsAppIcon />
-     <PhoneIcon/>
-     <ImageSlider />
+      <HomeSideBar />
+      <SimpleSlideshow />
+      <WhatsAppIcon />
+      <PhoneIcon/>
+      <ImageSlider />
     </>
   )
 }
