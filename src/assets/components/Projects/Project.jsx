f43c9f4bade2b1928df05b4d498d7c6cc5442451
ahmed shoieb <ahmd.shoiib@gmail.com>
2025-05-13 23:29:43 +0300

    finish footer &projects

--- src/assets/components/Projects/Project.jsx
+++ src/assets/components/Projects/Project.jsx
@@ -1,7 +1,11 @@
-export default function Project(){
+export default function Project({src , title}){
     return (
         <div>
-            <img src="" alt="" />
+        <div className="project">
+            <img className="project-image" src={src} alt="Project image" />
+            
+        </div>
+        <h2 className="project-title">{title}</h2>
         </div>
     )
-}
+} 
--- src/assets/components/Projects/ProjectsSection.jsx
+++ src/assets/components/Projects/ProjectsSection.jsx
@@ -1,13 +1,37 @@
+import React from 'react';
+import './ProjectsSection.css';
+import towD from '../../images/Projects/2d.jpg';
+import threeD from '../../images/Projects/3d.jpg';
+import supervision from '../../images/Projects/supervision.jpg';
+import Project from './Project';
+
 export default function ProjectsSection(){
+    const items = [
+        {
+            src: towD,
+            title: '2D Design',
+        },
+        {
+            src: threeD,
+            title: '3D Design',
+        },
+        {
+            src: supervision,
+            title: 'Execution Supervision',
+        },
+    ];
     return(
-        <div className="projects">
-            <h1>Projects</h1>
-            <p>Here are some of my projects.</p>
-            <ul>
-                <li>Project 1</li>
-                <li>Project 2</li>
-                <li>Project 3</li>
-            </ul>
+        <>
+        <h2 className="title-Services">Our Projects</h2>
+      <p className="subtitle-Services"></p>
+      
+      <div className="projects">
+            
+            {items.map((item, index) => (
+               <Project key={index} src={item.src} title={item.title} />
+            ))}
         </div>
+        </>
+        
     )
 }
